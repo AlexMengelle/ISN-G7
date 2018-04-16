893c071b7c31ab88b2387a175077d7357827df46
AlexMengelle <37932361+AlexMengelle@users.noreply.github.com>
2018-04-16 20:15:48 +0200

    Add files via upload

---
+++ testboutonju.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+  
+  <div class="bouton">
+  <input type="button" value="<" href="lien" </div>  
+
+  <?php $mois="janvier"; 
+  echo "".$mois;  ?>
+  <button> > </button> 
+ <body>
+ 
+ <?php 
+ echo '<p>Bonjour le monde</p>'; 
+ ?>
+ 
+ </body>
+</html>
