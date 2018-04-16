82345f22aa924587a87dbdaba77d9507ff901471
AlexMengelle <37932361+AlexMengelle@users.noreply.github.com>
2018-04-16 14:15:49 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+  <button> < </button> <body>Janvier</body> <button> > </button> 
+ <body>
+ <?php echo '<p>Bonjour le monde</p>'; ?>
+ </body>
+</html>
