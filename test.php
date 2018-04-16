a13c5d3409d2cece439bd8b0a8f41fc5aa7a3e47
AlexMengelle <37932361+AlexMengelle@users.noreply.github.com>
2018-04-16 16:56:45 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -2,8 +2,16 @@
  <head>
   <title>Test PHP</title>
  </head>
-  <button> < </button> <body>Janvier</body> <button> > </button> 
+  
+  <button> < </button>  
+  <?php $mois="janvier"; 
+  echo "".$mois;  ?>
+  <button> > </button> 
  <body>
- <?php echo '<p>Bonjour le monde</p>'; ?>
+ 
+ <?php 
+ echo '<p>Bonjour le monde</p>'; 
+ ?>
+ 
  </body>
 </html>
