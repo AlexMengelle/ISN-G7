22784e315b790c208aaef249bfce028ee2f7240d
AlexMengelle <37932361+AlexMengelle@users.noreply.github.com>
2018-04-20 15:08:40 +0200

    Add files via upload

--- calyt.php
+++ calyt.php
@@ -11,7 +11,22 @@
 		$dates=$date->getAll($annee); #recup toutes les dates du tableau		
 		#echo date('d/m/Y')
 		?>
-		
+		#mise en page
+		<div class="periodes">
+			<div class="annee">
+				<?php echo $annee; ?>
+			</div>
+			<div class="mois">
+				<ul>
+					<?php foreach ($date->mois as $mois); {
+						#recup des mois pour les mettre sous forme voulu
+					?>
+						<li><?php echo $mois; ?></li> #HTML
+					}
+					
+				</ul>
+			</div>
+		</div>
 		<?php print_r($dates);?>
 	</body>
 </html>
